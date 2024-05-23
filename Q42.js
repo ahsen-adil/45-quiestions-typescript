b0ac64ef76bfd7c9a546fbd6eef83791e177f685
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2024-05-23 06:27:14 +0500

    Add files via upload

---
+++ Q42.js
@@ -0,0 +1,17 @@
+"use strict";
+//42. Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+function show_magicians(magicians) {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+function make_great(magicians) {
+    let great_magicians = [];
+    for (let magician of magicians) {
+        great_magicians.push(`${magician} the Great`);
+    }
+    return great_magicians;
+}
+let magicianss = ["Merlin", "Gandalf", "Harry Houdini", "David Copperfield"];
+let great_magicians = make_great(magicianss);
+show_magicians(great_magicians);
---
+++ Q42.ts
@@ -0,0 +1,23 @@
+//42. Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+
+
+
+function show_magicians(magicians: string[]): void {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+
+function make_great(magicians: string[]): string[] {
+    let great_magicians: string[] = [];
+    for (let magician of magicians) {
+        great_magicians.push(`${magician} the Great`);
+    }
+    return great_magicians;
+}
+
+let magicianss: string[] = ["Merlin", "Gandalf", "Harry Houdini", "David Copperfield"];
+
+let great_magicians: string[] = make_great(magicianss);
+
+show_magicians(great_magicians);
